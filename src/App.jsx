73bbf2b0d3886bcafc96be2cfb1ea8b627064708
Carbon App <carbon@app.com>
2026-06-27 23:28:16 +0000

    fix: muscle map hue-rotate 210deg brightness 0.65 to match chart dark blue

--- src/App.jsx
+++ src/App.jsx
@@ -4518,7 +4518,7 @@ function BodyDiagram({muscleHeat,width=320,savedCount=0}){
       <img
         src={imgSrc}
         alt="Mapa muscular"
-        style={{width:"100%",height:"100%",objectFit:"contain",display:"block",filter:"sepia(1) saturate(4) hue-rotate(195deg) brightness(0.7)"}}
+        style={{width:"100%",height:"100%",objectFit:"contain",display:"block",filter:"sepia(1) saturate(4) hue-rotate(210deg) brightness(0.65)"}}
       />
     </div>
   );
